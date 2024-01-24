2b53ae0c2901bbd0b013a5993975e6ab8a85dc14
fox304 <nordmax69@gmail.com>
2024-01-25 02:40:22 +0700

    added delete and README

--- work5/file_1.py
+++ work5/file_1.py
@@ -16,31 +16,31 @@ API должен содержать следующие конечные точк
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from typing import List
 import pandas as pd
 from hw.work5.model import Tasks
 
 app = FastAPI()
-
 templates = Jinja2Templates(directory='hw/work5/templates')
-
 list_ = []
 
 
 @app.get('/tasks/', response_class=HTMLResponse)
 def list_of_tasks(requ: Request):
+	"""Выкладываем список задач"""
 	table = pd.DataFrame([vars(task) for task in list_]).to_html()
 	return templates.TemplateResponse('tasks_html.html', {'request': requ, 'table': table})
 
 
 @app.get('/tasks/{num}', response_model=Tasks)
 def select_task(num: int):
+	"""Выбор задачи по id"""
 	task_num = [task for task in list_ if task.id == num]
 	return task_num[0]
 
 
 @app.post('/tasks/', response_model=Tasks)
 def set_task(task: Tasks):
+	"""Добавляем задачи"""
 	task.id = len(list_) + 1
 	list_.append(task)
 	return task
@@ -48,8 +48,15 @@ def set_task(task: Tasks):
 
 @app.put('/tasks/{id}', response_model=Tasks)
 def update_task(id: int, task: Tasks):
-	task_num = [task_ for task_ in list_ if task.id == id]
+	"""Обновляем задачи"""
 	task.id = id
-	list_[id-1] = task
+	list_[id - 1] = task # если введут id, превышающий количество задач - выпадет ошибка
+	return task
+
+
+@app.delete('/tasks/{id}', response_model=Tasks)
+def cut_task(id: int, task: Tasks):
+	"""Удаляем задачи"""
+	list_.pop(id-1)
 	return task
 
